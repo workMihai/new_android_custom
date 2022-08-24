c5896fa651f7d0329c7dc70e43667eb80b5c58e1
mac <mac@iMacs-iMac.local>
2022-08-24 13:01:14 +0300

    add changes

--- android/src/main/kotlin/com/example/idenfy_sdk_flutter/IdenfySdkFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/idenfy_sdk_flutter/IdenfySdkFlutterPlugin.kt
@@ -5,10 +5,14 @@ import android.content.Intent
 import androidx.annotation.NonNull
 import android.graphics.Color
 import android.graphics.Rect
+import androidx.core.content.res.ResourcesCompat
+import com.facetec.sdk.FaceTecCancelButtonCustomization
+import com.facetec.sdk.FaceTecCustomization
 import com.google.gson.Gson
 import com.idenfy.idenfySdk.CoreSdkInitialization.IdenfyController
 import com.idenfy.idenfySdk.api.initialization.IdenfySettingsV2
 import com.idenfy.idenfySdk.api.liveliness.IdenfyLivenessUISettings
+import com.idenfy.idenfySdk.api.logging.IdenfySDKLoggingSettings
 import com.idenfy.idenfySdk.api.models.IdenfyOnBoardingViewTypeEnum
 import com.idenfy.idenfySdk.api.response.FaceAuthenticationResult
 import com.idenfy.idenfySdk.api.response.IdenfyIdentificationResult
@@ -46,13 +50,53 @@ class IdenfySdkFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, P
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else if (call.method == "start") {
       mResult = result
-//      val idenfyLivenessUISettings = IdenfyLivenessUISettings()
+      val idenfyLivenessUISettings = IdenfyLivenessUISettings()
+//      val livenessIdCheckCustomization = IdenfyLivenessUISettings.LivenessIdCheckCustomization();
+      var faceTechCustom : FaceTecCustomization = FaceTecCustomization()
+      faceTechCustom.feedbackCustomization.textColor = Color.parseColor("#FFC700")
+      faceTechCustom.frameCustomization.borderWidth = 0
+      faceTechCustom.guidanceCustomization.headerFont = ResourcesCompat.getFont(activity,R.font.sfpro_bold)
+      faceTechCustom.guidanceCustomization.retryScreenHeaderTextColor = Color.parseColor("#161617")
+      faceTechCustom.guidanceCustomization.retryScreenImageBorderColor = Color.TRANSPARENT
+      faceTechCustom.guidanceCustomization.retryScreenSubtextFont = ResourcesCompat.getFont(activity,R.font.sfpro_medium)
+      faceTechCustom.guidanceCustomization.retryScreenSubtextTextColor = Color.parseColor("#161617")
+      faceTechCustom.guidanceCustomization.buttonBackgroundDisabledColor = Color.parseColor("#A2A2A2")
+      faceTechCustom.guidanceCustomization.buttonBackgroundHighlightColor = Color.parseColor("#161617")
+      faceTechCustom.guidanceCustomization.buttonBackgroundNormalColor = Color.parseColor("#161617")
+      faceTechCustom.guidanceCustomization.foregroundColor = Color.parseColor("#161617")
+      faceTechCustom.feedbackCustomization.textColor = Color.parseColor("#161617")
+      faceTechCustom.guidanceCustomization.readyScreenHeaderTextColor = Color.parseColor("#161617")
+      faceTechCustom.guidanceCustomization.readyScreenSubtextFont = ResourcesCompat.getFont(activity,R.font.sfpro_medium)
+      faceTechCustom.guidanceCustomization.readyScreenSubtextTextColor = Color.parseColor("#161617")
+      faceTechCustom.cancelButtonCustomization.customImage = R.drawable.close_big
+      faceTechCustom.cancelButtonCustomization.location  = FaceTecCancelButtonCustomization.ButtonLocation.CUSTOM
+      var rect = Rect()
+      rect.left = 24
+      rect.bottom = 24
+      rect.right = 24
+      rect.top = 24
+      faceTechCustom.cancelButtonCustomization.customLocation = rect
+      faceTechCustom.ovalCustomization.strokeColor =  Color.parseColor("#FFC700")
+      faceTechCustom.ovalCustomization.progressColor1 = Color.parseColor("#FFC700")
+      faceTechCustom.ovalCustomization.progressColor2 = Color.parseColor("#FFC700")
+      faceTechCustom.overlayCustomization.showBrandingImage = false
+      faceTechCustom.feedbackCustomization.backgroundColors = Color.parseColor("#FFC700")
+      faceTechCustom.guidanceCustomization.buttonCornerRadius = 4
+      faceTechCustom.feedbackCustomization.textColor = Color.parseColor("#161617")
+      faceTechCustom.feedbackCustomization.textFont = ResourcesCompat.getFont(activity,R.font.sfpro_bold)
+      faceTechCustom.resultScreenCustomization.uploadProgressFillColor = Color.parseColor("#FFC700")
+      faceTechCustom.resultScreenCustomization.uploadProgressTrackColor = Color.parseColor("#161617")
+      faceTechCustom.resultScreenCustomization.customActivityIndicatorImage = R.drawable.empty
+      faceTechCustom.resultScreenCustomization.customActivityIndicatorRotationInterval = 0
+      faceTechCustom.resultScreenCustomization.foregroundColor = Color.parseColor("#FFFFFF")
+      idenfyLivenessUISettings.livenessCustomUISettings = faceTechCustom
+
       val idenfyUISettingsV2 = IdenfyUISettingsV2.IdenfyUIBuilderV2().withConfirmationView(
         IdenfyOnBoardingViewTypeEnum.SINGLE).withInstructions(IdenfyInstructionsType.DRAWER).build()
 
 
       val idenfySettingsV2 = IdenfySettingsV2.IdenfyBuilderV2()
-              .withAuthToken(call.argument<String>("authToken")!!).withIdenfyUISettingsV2(idenfyUISettingsV2)
+              .withAuthToken(call.argument<String>("authToken")!!).withIdenfyUISettingsV2(idenfyUISettingsV2).withLogging(IdenfySDKLoggingSettings.IdenfySDKLoggingEnum.FULL)
               .build()
 
       IdenfyController.getInstance().initializeIdenfySDKV2WithManual(this.activity, IdenfyController.IDENFY_REQUEST_CODE, idenfySettingsV2)
